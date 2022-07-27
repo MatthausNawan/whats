77a766e7e5963ddde9ec079b1bba13e28365948d
Matheus Sales <matheusalesbr@gmail.com>
2022-07-27 17:16:04 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<style>
+    .whats{
+        position: fixed; 
+        bottom: 40px; 
+        right: 40px;
+        
+        }
+    input{
+        background-color:#34af23;
+        border:none;
+        color: white;
+        cursor:pointer;
+        width: 200px;
+        padding: 15px;    
+        border-radius: 20px;
+    }
+    
+</style>
+
+<div class="whats">
+<a href="https://wa.me" target="_blank">
+   <input class="btn" type="submit"value="Falar com um especialista" title="Falar com um especialista" >
+</a>
+</div>
